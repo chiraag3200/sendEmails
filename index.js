6477cf8270ce1d1c3770fa2cd960a2f99291e8e7
chiraagmittal05 <chiraagmittal05@gmail.com>
2020-10-02 22:54:09 +0530

    final commit

---
+++ index.js
@@ -0,0 +1,122 @@
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+
+// scopes to access the required content
+var SCOPES = [
+  "https://mail.google.com/",
+  "https://www.googleapis.com/auth/gmail.modify",
+  "https://www.googleapis.com/auth/gmail.compose",
+  "https://www.googleapis.com/auth/gmail.send",
+];
+
+// The file to store the user's access and refresh tokens
+const TOKEN_PATH = "token.json";
+
+// Load client_secret and client_id from credentials.json
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Gmail API.
+  //   authorize(JSON.parse(content), listLabels);
+});
+
+// Create an OAuth2 client with the given credentials,
+// and then execute the given callback function.
+function authorize(credentials, callback) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if there is a previously stored token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+// Get and store new token after prompting for user authorization, and then
+// execute the given callback with the authorized OAuth2 client.
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here: ", (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error("Error retrieving access token", err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+function makeBody(to, from, subject, message) {
+  var str = [
+    'Content-Type: text/plain; charset="UTF-8"\n',
+    "MIME-Version: 1.0\n",
+    "Content-Transfer-Encoding: 7bit\n",
+    "to: ",
+    to,
+    "\n",
+    "from: ",
+    from,
+    "\n",
+    "subject: ",
+    subject,
+    "\n\n",
+    message,
+  ].join("");
+
+  var encodedMail = new Buffer(str)
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+  return encodedMail;
+}
+
+function sendMessage(auth) {
+  var raw = makeBody(
+    "chiraagmittal05@gmail.com",
+    "chiraag3200@gmail.com",
+    "Welcome",
+    "Thanks for registering"
+  );
+  const gmail = google.gmail({ version: "v1", auth });
+  gmail.users.messages.send(
+    {
+      auth: auth,
+      userId: "me",
+      resource: {
+        raw: raw,
+      },
+    },
+    function (err, response) {
+      return err || response;
+    }
+  );
+}
+
+fs.readFile("credentials.json", function processClientSecrets(err, content) {
+  if (err) {
+    console.log("Error loading client secret file: " + err);
+    return;
+  }
+  // Authorize a client with the loaded credentials, then call the
+  // Gmail API.
+  authorize(JSON.parse(content), sendMessage);
+});
